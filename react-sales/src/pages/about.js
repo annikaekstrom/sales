166872994f782d818c403155fba76b025ea04c1d
AnnikaEkstrom <ekstromannika@hotmail.com>
2018-07-24 20:48:16 +0200

    size of images and some information

--- react-sales/src/pages/about.js
+++ react-sales/src/pages/about.js
@@ -12,18 +12,18 @@ class About extends React.Component {
         <img id="profile" src={me} alt="" />
           <div className="about-text">
             <p id="aboutheadline">Send me your ideas and let us inspire sales!</p>
-            <p id="abouttext">
-              I have always been drawing and made work more fun
-              whereever I work; in bars, offices etc. It is a challenge
-              to have motivated team members and I am constantly
-              trying to do better in motivating my team.
-              Sales competitions can be extremely fun if you
-              make them creative and fun. This is wy I want to share
-              my ideas and also see your ideas! Take part of my ideas and how I have made playing a part of my sales strategy.
-              Send me your ideas and let us inspire each other!
+          </div>
+          <div className="abouttext">
+            I have always been drawing and made work more fun
+            whereever I work; in bars, offices etc. It is a challenge
+            to have motivated team members and I am constantly
+            trying to do better in motivating my team.
+            Sales competitions can be extremely fun if you
+            make them creative and fun. This is wy I want to share
+            my ideas and also see your ideas! Take part of my ideas and how I have made playing a part of my sales strategy.
+            Send me your ideas and let us inspire each other!
 
-            par.ekstrom@gmail.com
-            </p>
+          par.ekstrom@gmail.com
           </div>
       {/* </div> */}
     </div>
